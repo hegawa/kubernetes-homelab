034d21c3bc0ce7edbd1f16b74242f1f07a08407e
Vitisadi <vitisadi@gmail.com>
2023-05-14 17:28:02 +0000

    Fixed tests

--- my-app/src/App.test.js
+++ my-app/src/App.test.js
@@ -5,7 +5,4 @@ test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
-
-  const a = screen.getByText(/rdftcvgjhkbjnk/i);
-  expect(a).toBeInTheDocument();
 });
