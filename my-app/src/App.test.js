6ff842ccfe29fec15ba99eb4f5b869eb894b7486
Vitisadi <vitisadi@gmail.com>
2023-05-14 17:23:40 +0000

    Added testing

--- my-app/src/App.test.js
+++ my-app/src/App.test.js
@@ -5,4 +5,7 @@ test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
+
+  const a = screen.getByText(/rdftcvgjhkbjnk/i);
+  expect(a).toBeInTheDocument();
 });
